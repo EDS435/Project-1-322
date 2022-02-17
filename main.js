b3ca3e2918829c0acfd076ab051955a5978fd175
ESinson <55463616+ESinson@users.noreply.github.com>
2022-02-16 20:12:18 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+var mockDatabase = [
+    { _id: '123', name: 'Zeus Dice', published: true },
+    { _id: '583', name: 'Athena Dice', published: true },
+    { _id: '954', name: 'Ares Dice', published: false },
+    { _id: '384', name: 'Hephaestus Dice', published: false },
+    { _id: '183', name: 'Apollo Dice', published: true },
+    { _id: '007', name: 'Hypnos Dice', published: false },
+    { _id: '304', name: 'Nike Dice', published: true },
+    { _id: '729', name: 'Aphrodite Dice', published: false },
+    { _id: '734', name: 'Demeter Dice', published: true },
+];
+
+// Renders Current Selection of Products into Dom
+function renderList (results) {
+    const productDiv = document.querySelector('#products');
+    // clear out inner HTML to get rid of any older results
+    productDiv.innerHTML = '';
+    // Map each database record to a string containing the HTML for it's row
+    var products = results.map ((result, index) => {
+        return '<div>' + result.name + '</div>';
+    });
+    // Set the contents of the list to the new set of render HTML.
+    products.forEach((item) => {
+        productDiv.innerHTML += item; // += adds to HTML instead of overwriting it entirely.
+    });
+
+}
+
+renderList(mockDatabase);
+
+function togglePublished(showPublished) {
+    var filteredResults = mockDatabase.filter(function (result) {
+        return showPublished || result.published;
+    });
+    renderList(filteredResults);
+}
+document.querySelector('#published').addEventListener('change', function(event){
+    var value = event.target.value === 'true';
+    togglePublished(value);
+});
